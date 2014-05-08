ab98bfa0d6db52c78723af3654d2c2c629dc659c
Jamie <treblam@gmail.com>
2014-05-09 01:48:40 +0800

    add files.

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+// Get hello world page
+router.get('/helloworld', function(req, res) {
+	res.render('helloworld', {title: 'hello, world!'});
+});
+
+// Get less css
+/*router.get("*.less", function(req, res) {
+    var path = __dirname + req.url;
+    fs.readFile(path, "utf8", function(err, data) {
+        if (err) throw err;
+        less.render(data, function(err, css) {
+            if (err) throw err;
+            res.header("Content-type", "text/css");
+            res.send(css);
+        });
+    });
+});*/
+
+module.exports = router;
