ceadb35b09a10057879520283588251eea7cebbd
Jamie <treblam@gmail.com>
2014-06-05 19:12:13 +0800

    #deploy test

--- routes/index.js
+++ routes/index.js
@@ -106,10 +106,7 @@ router.get('/', function(req, res) {
     console.log('session: ');
     console.log(req.session);
     console.log(req.user != null);
-//    res.render('index', { title: '首页', user: req.user });
-    res.send('这是首页');
-
-    
+    res.render('index', { title: '首页', user: req.user });
 });
 
 // Get less css
