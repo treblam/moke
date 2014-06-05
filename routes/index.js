10ac29cff604bd4f19ba65a1f19d057a8480ce8e
Jamie <treblam@gmail.com>
2014-06-05 19:51:58 +0800

    #deploy test

--- routes/index.js
+++ routes/index.js
@@ -107,6 +107,7 @@ router.get('/', function(req, res) {
     console.log(req.session);
     console.log(req.user != null);
     console.log('test');
+    console.log('test2');
     res.render('index', { title: '首页', user: req.user });
 });
 
